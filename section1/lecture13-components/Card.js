3b948731afe1474a47311e64ffc60252fbb14830
Federico Personale <maiofederico7@gmail.com>
2026-02-23 16:31:16 +0100

    Modificati file lecture 13 e 14

--- section1/lecture13-components/Card.js
+++ section1/lecture13-components/Card.js
@@ -4,11 +4,11 @@ function Card(){
             <div className="col s2">
                 <div className="card hoverable small">
                     <div className="card-image">
-                        <img src="http://lorempixel.com/400/400/nature/" />
+                        <img src="https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" />
                     </div>
                     <div className="card-content">
-                        <p>React From the Beginning</p>
-                        <p>Robert Bunch</p>
+                        <p>React from the beginning</p>
+                        <p>Robert Munch</p>
                     </div>
                     <div className="card-action">
                         <a href="#">$9.99</a>
--- section1/lecture14-props/Card.js
+++ section1/lecture14-props/Card.js
@@ -1,19 +1,20 @@
-function Card(props){
-    console.log(props);
+function Card(){
     return(
-        <div className="col s2">
-            <div className="card hoverable small">
-                <div className="card-image">
-                    <img src="http://lorempixel.com/400/400/nature/" />
+        <div className="row">
+            <div className="col s2">
+                <div className="card hoverable small">
+                    <div className="card-image">
+                        <img src="https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" />
+                    </div>
+                    <div className="card-content">
+                        <p>React from the beginning</p>
+                        <p>Robert Munch</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">$9.99</a>
+                    </div>
                 </div>
-                <div className="card-content">
-                    <p>{props.title}</p>
-                    <p>{props.name}</p>
-                </div>
-                <div className="card-action">
-                    <a href="#">$9.99</a>
-                </div>
-            </div>
-        </div>	
+            </div>	
+        </div>
     )
 }
