52103173e5a908f7609fd275034948c0bc8aa477
Federico Personale <maiofederico7@gmail.com>
2026-02-23 16:52:23 +0100

    Modificati file lecture 14

--- section1/lecture14-props/Card.js
+++ section1/lecture14-props/Card.js
@@ -1,20 +1,19 @@
-function Card(){
+function Card(props){
+    console.log(props);
     return(
-        <div className="row">
-            <div className="col s2">
-                <div className="card hoverable small">
-                    <div className="card-image">
-                        <img src="https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" />
-                    </div>
-                    <div className="card-content">
-                        <p>React from the beginning</p>
-                        <p>Robert Munch</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">$9.99</a>
-                    </div>
+        <div className="col s2">
+            <div className="card hoverable small">
+                <div className="card-image">
+                    <img src="https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" />
                 </div>
-            </div>	
-        </div>
+                <div className="card-content">
+                    <p>{props.name}</p>
+                    <p>{props.title}</p>
+                </div>
+                <div className="card-action">
+                    <a href="#">$9.99</a>
+                </div>
+            </div>
+        </div>	
     )
 }
