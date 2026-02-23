8a12182ea6e63fa1f945fd6b2af003c97bf10a89
Federico Personale <maiofederico7@gmail.com>
2026-02-23 18:30:49 +0100

    Modificati file lecture 15

--- section1/lecture15-practiceProps/Card.js
+++ section1/lecture15-practiceProps/Card.js
@@ -1,1 +1,25 @@
 // Card component
+// import "data.js";
+// import "app.js";
+
+
+function Card(props){
+    console.log(props);
+    const img = props.image;
+    return(
+        <div className="col s2">
+            <div className="card hoverable small">
+                <div className="card-image">
+                    <img src={img}/>
+                </div>
+                <div className="card-content">
+                    <p>{props.course}</p>
+                    <p>{props.instructor}</p>
+                </div>
+                <div className="card-action">
+                    <a href="#">$9.99</a>
+                </div>
+            </div>
+        </div>	
+    )
+}
--- section1/lecture15-practiceProps/app.js
+++ section1/lecture15-practiceProps/app.js
@@ -1,1 +1,13 @@
 // ReactDOM.render here
+// import "Card.js";
+// import "data.js";
+
+ReactDOM.render(
+    <div className="row">
+        <Card name = {data[0].course} title ={data[0].instructor} image={data[0].image}/>
+        <Card name = {data[1].course} title ={data[1].instructor} image={data[0].image}/>
+        <Card name = {data[2].course} title ={data[2].instructor} image={data[0].image}/>
+        <Card name = {data[3].course} title ={data[3].instructor} image={data[0].image}/>
+    </div>,
+    document.getElementById('root')
+);
