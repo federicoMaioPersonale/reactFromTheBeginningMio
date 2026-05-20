df1e80b5c164a33405269e38e50a55be184cd73a
Federico Personale <maiofederico7@gmail.com>
2026-05-20 21:57:25 +0200

    Aggiunti commenti

--- section3/src/App.js
+++ section3/src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      temp:""
+    }
+    // non possiamo usare questo approccio perchè comunque la response 
+    // non si sa quando arriva e this.state.temp potrebbe essere letto e compilato
+    // prima della response, quindi tocca usare un altro metodo
+    
+    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=19563a6d4ec2155deb4ba978d9d1d8dc'
+    // // stiamo facendo una HTTP request GET verso la url. quando la richiesta è
+    // // completata, verrà eseguito il codice nel .then
+    // axios.get(url).then((resp)=>{
+    //   // console.log(resp.data);
+    //   this.state = {
+    //     temp: resp.state.data.temp
+    //   }
+    // })
+  }
+  
+  // compoonentDidMount è un metodo di Component, nativo, che ci permette di fare operazioni
+  // quando il componente App in questo caso, perchè siamo su App, ha finito di fare il primo rendering,
+  // componentDidMount viene fatto partire. parte così anche la richiesta: dopo che è stata fatta la richiesta
+  // viene ri-fatto un render()
+  componentDidMount(){
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=19563a6d4ec2155deb4ba978d9d1d8dc'
+    // stiamo facendo una HTTP request GET verso la url. quando la richiesta è
+    // completata, verrà eseguito il codice nel .then
+    axios.get(url).then((resp)=>{
+      this.setState({
+        temp: resp.state.data.temp
+      })
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>{this.state.temp}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
