e9a5373a990c5fdac98f0c6ea3a0fe8ad4cd8d9d
Federico Personale <maiofederico7@gmail.com>
2026-05-21 15:46:39 +0200

    Aggiunti commenti, modificae app e index

--- section3/src/App.js
+++ section3/src/App.js
@@ -5,15 +5,24 @@ import React, { Component } from 'react';
 
 class App extends Component {
   constructor(){
+    console.log("constructor running...")
     super();
     this.state = {
-      temp:""
+      temp:"",
+      cityName: "",
+      weather: "",
+      high:"",
+      low:"",
+      icon:"",
+      isRaining:""
     }
+    
     // non possiamo usare questo approccio perchè comunque la response 
     // non si sa quando arriva e this.state.temp potrebbe essere letto e compilato
     // prima della response, quindi tocca usare un altro metodo
     
     // metti url qui come commento
+    // const url = 
     // // stiamo facendo una HTTP request GET verso la url. quando la richiesta è
     // // completata, verrà eseguito il codice nel .then
     // axios.get(url).then((resp)=>{
@@ -29,20 +38,93 @@ class App extends Component {
   // componentDidMount viene fatto partire. parte così anche la richiesta: dopo che è stata fatta la richiesta
   // viene ri-fatto un render()
   componentDidMount(){
+    console.log("component did mount");
+    // qua mettiamo un valore placeholder perchè all'inizio non abbiamo una città
+    this.getCityWeather('London');
+    // mettiamo qui le cose per la modale perchè potrebbe non essere stata inizializzata
+    var elems = document.querySelectorAll('.modal');
+    // si scrive window.M.... perchè se scrivi solo M eccetera, M non lo riconosce in quanto
+    // esso è legato all'oggetto window
+    // inizializz la modale di Materialize (M)
+    var instances = M.Modal.init(elems, options);;
+  }
+
+  // prevProps e prevState sono le proprietà e lo stato prima dell'update del Componente
+  // snapshot ti restituisce delle info del tipo: a che posizione era il mouse prima di update
+  // componentDidUpdate(prevProps,prevState,snapshot){
+  //   console.log("component did update");
+  //   console.log(prevProps);
+  //   console.log(prevState);
+  //   console.log(snapshot);
+  // }
+
+  // runna dopo ogni singolo render. render va dopo i cambi di stato o proprietà
+  componentDidUpdate(prevProps,prevState){
+    // condizione che se lo stato precedente è diverso
+    if(this.state.weather !== prevState.weather){
+      console.log("component did update");
+      console.log(prevProps);
+      console.log(prevState);
+      console.log(snapshot);
+      const isRaining = this.state.weather.includes("rain");
+      if(isRaining){
+        this.setState({
+          isRaining: "Rain Rain go away"
+        })
+      }
+    }
+  }
+
+  searchCity = (e)=>{
+    e.preventDefault();
+    console.log("Form Submitted");
+    const city = document.getElementById('city').value;
+    this.getCityWeather(city);
+  }
+
+  getCityWeather = (city)=>{
         // metti url qui
-    // stiamo facendo una HTTP request GET verso la url. quando la richiesta è
-    // completata, verrà eseguito il codice nel .then
+    const url = `robaApi${city}.org`;
+    // lo spostiamo qui in quanto ora ci interessa usare la url modificata con city in dinamica
     axios.get(url).then((resp)=>{
       this.setState({
-        temp: resp.state.data.temp
+        temp: resp.data.main.temp,
+        high: resp.data.main.temp_max,
+        low: resp.data.main.temp_max,
+        weather: resp.data.weather[0].description,
+        icon: resp.data.weather[0].icon,
+        cityName : resp.data.name
       })
     })
   }
 
   render(){
+    const iconUrl = `openweathersito/${this.state.icon}.png`;
     return (
       <div className="App">
-        <h1>{this.state.temp}</h1>
+        <div className='row'>
+          <div className='col s6 offset-s3'>
+            <h1>{this.state.temp}</h1>
+            <h1>{this.state.isRaining}</h1>
+            {/* <!-- Modal Trigger --> */}
+            <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+            <form onSubmit={this.searchCity}>
+              <input type='text' placeholder='Enter a City Name'/>
+            </form>
+            {/* <!-- Modal Structure --> */}
+            <div id="modal1" className="modal">
+              <div className="modal-content">
+                <h4>{this.state.cityName}</h4>
+                <p>High:{this.state.high} - Low:{this.state.low}</p>
+                {/* <p>{this.state.weather} {this.state.icon}</p> */}
+                <p>{this.state.weather}<img>{iconUrl}</img></p>
+              </div>
+              <div className="modal-footer">
+                <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
