81ada714abbea436a4a6c46df11fc6bdf6236223
Federico Personale <maiofederico7@gmail.com>
2026-05-22 17:05:48 +0200

    Fatto un create react app di aws flash cards

--- aws_flash_cards/src/App.js
+++ aws_flash_cards/src/App.js
@@ -1,41 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import QuizBar from './components/QuizBar';
-import FlashCard from './components/FlashCard'
 
-class App extends Component{
-	constructor(){
-		super();
-		this.state = {
-      cardStyle: "Random",
-      ready: false,
-		}
-  }
-
-  userChoice = (cardStyle)=>{
-    this.setState({
-      cardStyle,
-      ready: false
-    })
-  }
-
-  nowReady = ()=>{
-    this.setState({
-      ready: true
-    })
-  }
-
-  render(){
-    console.log(this.state.cardStyle);
-    return (
-      <div className="App align-items-center d-flex">
-        <div className="container">
-          <QuizBar userChoice={this.userChoice} />
-          <FlashCard cardStyle={this.state.cardStyle} nowReady={this.nowReady} ready={this.state.ready}  />
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
