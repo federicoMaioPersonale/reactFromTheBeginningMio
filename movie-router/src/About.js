365b5d7fe428d1f9419fa6f540788273f5d4adbc
Federico Personale <maiofederico7@gmail.com>
2026-05-24 12:30:57 +0200

    Movie router

--- movie-router/src/About.js
+++ movie-router/src/About.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
 
 function About(props){
-    console.log(props);
     return(
-        <h1>About page!</h1>
+        <h1>{props.title}</h1>
     )
 }
 
